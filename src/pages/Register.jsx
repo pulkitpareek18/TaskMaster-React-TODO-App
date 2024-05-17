cf16184fc7924e2c72e3c993a134404fd77de80b
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2024-05-17 21:38:27 +0530

    Add: About Us and Contact Us Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -69,7 +69,7 @@ const Register = () => {
                             </div>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" disabled={loading}>Register</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</Link>
+                                Already have an account? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</Link>
                             </p>
                         </form>
                     </div>
